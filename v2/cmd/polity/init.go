4b49d3566c60df31f95d9e1e8975e4cc3d86eb96
code_monk <sean.r.macdonald@gmail.com>
2025-07-07 07:11:23 -0400

    wip

--- v2/cmd/polity/init.go
+++ v2/cmd/polity/init.go
@@ -8,18 +8,33 @@ import (
 	"github.com/sean9999/polity/v2"
 )
 
+func dieOn(err error, env hermeti.Env) {
+	if err != nil {
+		fmt.Fprintln(env.ErrStream, err)
+		env.Exit(1)
+	}
+}
+
 func _init(e hermeti.Env, _ *app) {
 
 	udpnet := new(polity.LocalUDP4Net)
 
 	p, err := polity.NewPrincipal(e.Randomness, udpnet)
-	if err != nil {
-		fmt.Println(e.ErrStream, err)
-		e.Exit(1)
-		return
-	}
+	dieOn(err, e)
+	// if err != nil {
+	// 	fmt.Println(e.ErrStream, err)
+	// 	e.Exit(1)
+	// 	return
+	// }
 
-	brokenHill, err := polity.PrincipalFromFile("testdata/broken-hill.pem", new(polity.LocalUDP4Net))
+	brokenHill, err := polity.PrincipalFromFile("../../testdata/broken-hill.pem", new(polity.LocalUDP4Net))
+	// if err != nil {
+	// 	fmt.Println(e.ErrStream, err)
+	// 	e.Exit(1)
+	// 	return
+	// }
+	dieOn(err, e)
+
 	p.AddPeer(brokenHill.AsPeer())
 
 	p.Connect()
