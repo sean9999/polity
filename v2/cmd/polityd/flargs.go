7135077d710b4413aa1bc63eac4e159c0fb7bec4
Sean M <Sean.macdonald@hardrockdigital.com>
2025-07-16 06:57:54 -0400

    wip

--- v2/cmd/polityd/flargs.go
+++ v2/cmd/polityd/flargs.go
@@ -2,72 +2,48 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/sean9999/polity/v2/udp4"
 	"os"
 
 	"github.com/sean9999/polity/v2"
 )
 
-type joinPeer struct {
-	Peer *polity.Peer[*udp4.Network]
-}
-
-func (join *joinPeer) String() string {
-	if join == nil || join.Peer == nil {
-		return ""
-	}
-	username := join.Peer.ToHex()
-	addr := join.Peer.Addr.String()
-	protocol := join.Peer.Addr.Network()
-	return fmt.Sprintf("%s://%s@%s", protocol, username, addr)
-}
-
-func (join *joinPeer) Set(s string) error {
-	peer2, err := polity.PeerFromString(s, &udp4.Network{})
-	if err != nil {
-		return err
-	}
-
-	join.Peer = polity.NewPeer[*udp4.Network]()
-
-	join.Peer.Addr = peer2.Addr
-	join.Peer.Peer = peer2.Peer
-	return nil
-}
-
-type self *polity.Principal[*udp4.Network]
-
-type privConf struct {
-	me       self
-	filename string
-}
-
-func (m *privConf) String() string {
-	return m.filename
-}
-
-func (m *privConf) Set(s string) error {
-	bin, err := os.ReadFile(s)
-	if err != nil {
-		return err
-	}
-	me, err := polity.PrincipalFromPEM(bin, &udp4.Network{})
-	m.me = me
-	m.filename = s
-	return err
-}
-
-func parseFlargs() (*joinPeer, *privConf, uint8, error) {
-	joiner := new(joinPeer)
+func parseFlargs() (*polity.Peer[*udp4.Network], *polity.Principal[*udp4.Network], string, uint8, error) {
 
 	f := flag.NewFlagSet("fset", flag.ContinueOnError)
-	f.Var(joiner, "join", "node to join")
 
-	self := new(privConf)
-	f.Var(self, "conf", "private key PEM file representing self")
+	var me *polity.Principal[*udp4.Network] = nil
+	var joinPeer *polity.Peer[*udp4.Network] = nil
+
+	var confFileName string
+
+	f.Func("conf", "config file", func(filename string) error {
+		if len(filename) == 0 {
+			return nil
+		}
+		confFileName = filename
+		fileData, err := os.ReadFile(filename)
+		if err != nil {
+			return err
+		}
+		me, err = polity.PrincipalFromPEM(fileData, new(udp4.Network))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	f.Func("join", "peer to join", func(filename string) error {
+		if len(filename) == 0 {
+			return nil
+		}
+		var err error = nil
+		joinPeer, err = polity.PeerFromString(filename, new(udp4.Network))
+		return err
+	})
+
 	verbosity := f.Uint("verbosity", 0, "verbosity level")
 
 	err := f.Parse(os.Args[1:])
-	return joiner, self, uint8(*verbosity), err
+	return joinPeer, me, confFileName, uint8(*verbosity), err
 }
--- v2/cmd/polityd/main.go
+++ v2/cmd/polityd/main.go
@@ -5,65 +5,34 @@ import (
 	"errors"
 	"fmt"
 	"github.com/sean9999/polity/v2"
+
 	"github.com/sean9999/polity/v2/subj"
 	"github.com/sean9999/polity/v2/udp4"
 	"os"
 )
-
-//var NoUUID uuid.UUID
-//
-//func dieOn(stream io.Writer, err error) {
-//	if err != nil {
-//		err = pear.AsPear(err, 3)
-//		fmt.Fprintln(stream, err)
-//		pear.NicePanic(stream)
-//		os.Exit(1)
-//	}
-//}
 
 func main() {
 
 	//	exit signal
 	done := make(chan error)
 
-	join, meConf, _, err := parseFlargs()
+	join, me, meConf, _, err := parseFlargs()
 	if err != nil {
 		panic(err)
 	}
 	//	initialize a new or existing Principal
-	var p *polity.Principal[*udp4.Network]
-	if meConf == nil || meConf.me == nil {
-		p, err = polity.NewPrincipal(rand.Reader, new(udp4.Network))
-		if err != nil {
-			done <- err
-		}
-	} else {
-		data, err := os.ReadFile(meConf.String())
-		if err != nil {
-			panic(err)
-		}
-
-		p, err = polity.PrincipalFromPEM(data, new(udp4.Network))
-		if err != nil {
-			panic(err)
-		}
-
-	}
-	if p == nil {
-		if err != nil {
-			panic(errors.New("no principal"))
-		}
-	} else {
-		err = p.Connect()
+	if me == nil {
+		me, err = polity.NewPrincipal(rand.Reader, new(udp4.Network))
 		if err != nil {
 			panic(err)
 		}
 	}
+	_ = me.Connect()
 
 	// handle inbox
 	go func() {
-		for e := range p.Inbox {
-			onEnvelope(p, e, meConf.String())
+		for e := range me.Inbox {
+			onEnvelope(me, e, meConf)
 		}
 		//	once the inbox channel is closed, we assume it's time to exit
 		done <- errors.New("goodbye")
@@ -71,13 +40,13 @@ func main() {
 
 	//	knowledge-base events
 	go func() {
-		for ev := range p.Peers.Events {
+		for ev := range me.Peers.Events {
 			msg := fmt.Sprintf("%s on %s was %v and is now %v", ev.Action, ev.Key.Nickname(), ev.OldVal.IsAlive, ev.NewVal.IsAlive)
 			prettyNote(msg)
 		}
 	}()
 
-	bootId, err := boot(p)
+	bootId, err := boot(me)
 	//	if we can't send a boot up message to ourselves, we must explain ourselves and die
 	if err != nil {
 		panic(err)
@@ -85,7 +54,7 @@ func main() {
 
 	//	if our process was started with a "-join=pubkey@address" flag, try to join that peer
 	if join.Peer != nil {
-		err = sendFriendRequest(p, join.Peer, bootId)
+		err = sendFriendRequest(me, join, bootId)
 		//	if we can't join a peer, we should kill ourselves.
 		if err != nil {
 			panic(err)
@@ -93,17 +62,17 @@ func main() {
 	}
 
 	//	send out "hello, I'm alive" to all my friends (if I have any)
-	for pubKey, info := range p.Peers.Entries() {
+	for pubKey, info := range me.Peers.Entries() {
 		fmt.Printf("sending hello to %s\n", pubKey)
 
-		e := p.Compose(nil, info.Recompose(pubKey), bootId)
+		e := me.Compose(nil, info.Recompose(pubKey), bootId)
 		e.Subject(subj.Hello)
-		_ = send(p, e)
+		_ = send(me, e)
 
 		//	NOTE: should we assume the peer is dead until we hear back?
 		//	This might be too chatty. It requires the peer to respond.
 		info.IsAlive = false
-		_ = p.Peers.Set(pubKey, info)
+		_ = me.Peers.Set(pubKey, info)
 
 	}
 
