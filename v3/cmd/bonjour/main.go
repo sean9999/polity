5b7f27f5dfe5fcd0e2263ef564a55d506c1aa335
Sean M <Sean.macdonald@hardrockdigital.com>
2026-01-06 20:27:43 -0500

    wip

--- v3/cmd/bonjour/main.go
+++ v3/cmd/bonjour/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/grandcat/zeroconf"
@@ -24,13 +27,36 @@ func main() {
 		log.Println("No more entries.")
 	}(entries)
 
+	go advertise()
+
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
 	defer cancel()
-	err = resolver.Browse(ctx, "_ssh._tcp", "local.", entries)
+	err = resolver.Browse(ctx, "_foo._tcp", "local.", entries)
 	if err != nil {
 		log.Fatalln("Failed to browse:", err.Error())
 	}
 
 	<-ctx.Done()
 
+}
+
+func advertise() {
+
+	server, err := zeroconf.Register("GoZeroconf", "_foo._tcp", "local.", 42424, []string{"txtv=0", "lo=1", "la=2"}, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer server.Shutdown()
+
+	// Clean exit.
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	select {
+	case <-sig:
+		// Exit by user
+	case <-time.After(time.Second * 120):
+		// Exit by timeout
+	}
+
+	log.Println("Shutting down.")
 }
